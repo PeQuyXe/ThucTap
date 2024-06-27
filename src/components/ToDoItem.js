8e15cdceac61f732d6d5c7d40c8e6a6864ea202a
PeQuyXe <cuongpham17072002@gmail.com>
2024-06-27 23:29:15 +0700

    todolist-basic

--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -3,16 +3,16 @@ import './Addbutton.css';
 
 function ToDoItem({ task, index, toggleTask, removeTask }) {
   return (
-    <div>
-      <span style={{ color: task.completed ? 'rgba(0, 0, 0, 0.5)' : 'rgba(0, 0, 0, 1)' }}>
+    <div className='home'>
+      <span style={{color: task.completed ? 'gray' : 'black' }}>
         {task.text}
       </span>
-      {'  '}
-      <button type='button' className='btn btn-primary' onClick={() => toggleTask(index)}>
+      {' '}
+      <button style={{color: 'blue'}} onClick={() => toggleTask(index)}>
         {task.completed ? 'Hoàn Tác' : 'Hoàn Thành'}
       </button>
-      {'  '}
-      <button type ='button' className ='btn btn-danger' onClick={() => removeTask(index)}>Xóa</button>
+      {' '}
+      <button  style={{color: 'red'}}  onClick={() => removeTask(index)}>Xóa</button>
     </div>
   );
 }
